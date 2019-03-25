2abee88997213e2b6c0960d9ff00f152c82a853e
doiphode <s.doiphode@qmul.ac.uk>
2019-03-25 13:44:30 +0000

    Clean and load data to employees array

--- index.php
+++ index.php
@@ -1,1 +1,9 @@
-<?php echo "Hello KCL!!!" ?>
+<?php
+    //load and clean data.js to retrive only json string
+    $data = file_get_contents('./resources/data.js');
+    $data = str_replace(['var data = ', ';',], '', $data);
+    //decode json to produce employees array
+    $employees = json_decode($data,true);
+    echo "<pre>";
+    var_dump($employees);
+?>
